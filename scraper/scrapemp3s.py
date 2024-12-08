617d0e1e3f29be9cf82ca2aec1b5d3b2d10bc044
Pieter <61529343+mcbobsk1@users.noreply.github.com>
2024-12-08 13:35:44 +1300

    only scrape mp3s

--- scraper/scrapemp3s.py
+++ scraper/scrapemp3s.py
@@ -7,22 +7,23 @@ mp3Files = []
 
 directory = (".\\mp3_library")
 for name in os.listdir(directory):
-    filepath = os.path.join(directory, name)
-    file = TinyTag.get(filepath)
-    filename = file.title
-    #some songs have no title metadata, relying on filename. So if this is the case, we extract that instead
-    if filename is None:
-        filename = filepath[filepath.rindex("\\")+1:filepath.rindex(".")]
-    if file.artist is None:
-        file.artist = "Unknown"
-    audio = {
-        "name": filename,
-        "filepath": filepath,
-        "duration_seconds": file.duration,
-        "artist_name": file.artist,
-        "genre": ""
-    }
-    mp3Files.append(audio)
+    if name.endswith('.mp3'):   
+        filepath = os.path.join(directory, name)
+        file = TinyTag.get(filepath)
+        filename = file.title
+        #some songs have no title metadata, relying on filename. So if this is the case, we extract that instead
+        if filename is None:
+            filename = filepath[filepath.rindex("\\")+1:filepath.rindex(".")]
+        if file.artist is None:
+            file.artist = "Unknown"
+        audio = {
+            "name": filename,
+            "filepath": filepath,
+            "duration_seconds": file.duration,
+            "artist_name": file.artist,
+            "genre": ""
+        }
+        mp3Files.append(audio)
 
 #write our data to a json file
 f = open("./scraper/listofmp3s.json", "w")
