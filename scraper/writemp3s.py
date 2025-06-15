6a824f2e7fdb685e0b015e41bd5f9d3233d3e049
Pieter Brassey-Van Der Haas <61529343+pbvdh@users.noreply.github.com>
2025-06-15 12:01:16 +1200

    Update writemp3s.py

--- scraper/writemp3s.py
+++ scraper/writemp3s.py
@@ -3,7 +3,7 @@ import requests
 
 
 #use our scraped json file of mp3 metadata and use our own api to write these to the database
-with open('./scraper/listofmp3s.json', 'r') as file:
+with open('listofmp3s.json', 'r') as file:
     data = json.load(file)
 
 url = "http://localhost:3000/songs"
