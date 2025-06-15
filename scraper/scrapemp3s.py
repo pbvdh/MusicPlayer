325bcbe323980aa244c92d5ed17de828c50b2f6a
Pieter Brassey-Van Der Haas <61529343+pbvdh@users.noreply.github.com>
2025-06-15 12:00:48 +1200

    Update scrapemp3s.py

--- scraper/scrapemp3s.py
+++ scraper/scrapemp3s.py
@@ -5,7 +5,7 @@ from tinytag import TinyTag
  #loop through mp3 files in server directory, and record the metadata we want to use to form our records in the database
 mp3Files = []
 
-directory = (".\\mp3_library")
+directory = ("../mp3_library")
 for name in os.listdir(directory):
     if name.endswith('.mp3'):   
         filepath = os.path.join(directory, name)
@@ -26,7 +26,7 @@ for name in os.listdir(directory):
         mp3Files.append(audio)
 
 #write our data to a json file
-f = open("./scraper/listofmp3s.json", "w")
+f = open("listofmp3s.json", "w")
 string = json.dumps(mp3Files, indent=2)
 f.write(string)
 f.close()
